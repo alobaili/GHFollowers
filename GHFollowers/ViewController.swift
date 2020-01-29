cbf9f705a96ad429d8810a17fa1e0048ded8c6ba
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-29 22:59:54 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 29/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
