c78b4ea2d9262509dbd26f99e18ef0452821f76c
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-31 16:03:38 +0300

    create User and Follower model types

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,17 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 31/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    
+    var login: String
+    var avatarUrl: String
+    
+    
+}
---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 31/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+    
+    
+}
