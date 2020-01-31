e45cf4d98c1b08393f008e354fb753d44740e7f0
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-31 13:29:58 +0300

    create `GFTitleLabel`

---
+++ GHFollowers/GFTitleLabel.swift
@@ -0,0 +1,40 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 31/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+}
