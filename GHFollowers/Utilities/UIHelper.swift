1e15dfab1a82c327074c367f32992987d12864ea
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-31 20:28:39 +0300

    refactor and clean some code
    - prevent a potential memory leak in `getFollowers` network call by making `self` weak.
    - refactor creating the flow layout to another file called UIHelper because `FollowersListViewController` doesn't really need to know about its implementation details.

---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,28 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 31/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    
+}
