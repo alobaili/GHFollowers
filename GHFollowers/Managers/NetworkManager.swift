fc596e93412eea8215b836cb6cb9ad29dddb91ff
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-31 17:03:35 +0300

    create NetworkManager with getFollowers function

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 31/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseUrl = "https://api.github.com/users/"
+    
+    
+    private init() {}
+    
+    func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, String?) -> Void) {
+        let endpoint = baseUrl + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(nil, "This username created an invalid request. Please try again.")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let _ = error {
+                completion(nil, "Unable to complete your request. Please check your internet connection.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(nil, "Invalid response from the server. Please try again.")
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, "The data recieved from the server was invalid. Please try again.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completion(followers, nil)
+            } catch {
+                completion(nil, "The data recieved from the server was invalid. Please try again.")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+}
--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -18,6 +18,16 @@ class FollowersListViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+            
+            print("Followers count: \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
