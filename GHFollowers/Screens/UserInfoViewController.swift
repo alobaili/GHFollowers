bc04cf2f934a894b96fe6e1cf5c4c260da16c29f
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-02-01 13:13:28 +0300

    create a user info screen and pass the follower username to it

---
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoViewController.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 01/02/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    
+    var username: String!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
+        navigationItem.rightBarButtonItem = doneButton
+        print(username!)
+    }
+    
+    @objc func dismissViewController() {
+        dismiss(animated: true)
+    }
+    
+
+}
