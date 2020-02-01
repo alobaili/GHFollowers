ae5cd8ac7d77704a02a35b09e5296c1f8900f402
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-02-01 11:51:32 +0300

    show activity indicator when loading followers

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -52,8 +52,11 @@ class FollowersListViewController: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] (result) in
             guard let self = self else { return }
+            
+            self.dismissLoadingView()
             
             switch result {
                 case .success(let followers):
