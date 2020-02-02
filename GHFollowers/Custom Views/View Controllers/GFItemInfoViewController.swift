1f99a8c29f881aabebe0012b781d9f11d72b7cf4
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-02-02 20:32:17 +0300

    create the super class GFItemInfoViewController
    this class represent a reusable item info view that can be subclassed

---
+++ GHFollowers/Custom Views/View Controllers/GFItemInfoViewController.swift
@@ -0,0 +1,64 @@
+//
+//  GFItemInfoViewController.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 02/02/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class GFItemInfoViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let itemInfoView1 = GFItemInfoView()
+    let itemInfoView2 = GFItemInfoView()
+    let actionButton = GFButton()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoView1)
+        stackView.addArrangedSubview(itemInfoView2)
+    }
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        // stackView
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        // actiobButton
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+
+}
