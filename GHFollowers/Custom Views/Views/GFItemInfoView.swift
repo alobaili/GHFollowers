5682ab5a3aba60cb845f6cdd53cade9b076227f4
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-02-02 20:16:00 +0300

    create the item info view

---
+++ GHFollowers/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,86 @@
+//
+//  GFItemInfoView.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 01/02/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+enum ItemInfoType {
+    case repos, gists, followers, following
+}
+
+
+class GFItemInfoView: UIView {
+
+    let symbolImageView = UIImageView()
+    let titleLabel = GFTitleLabel(textAlignment: .left, fontSize: 14)
+    let countLabel = GFTitleLabel(textAlignment: .center, fontSize: 14)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(symbolImageView)
+        addSubview(titleLabel)
+        addSubview(countLabel)
+        
+        symbolImageView.translatesAutoresizingMaskIntoConstraints = false
+        symbolImageView.contentMode = .scaleAspectFill
+        symbolImageView.tintColor = .label
+        
+        // symbolImageView
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        // titleLabel
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+        
+        // countLabel
+        NSLayoutConstraint.activate([
+            countLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
+            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            countLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+    
+    func set(itemInfoType: ItemInfoType, withCount count: Int) {
+        switch itemInfoType {
+            case .repos:
+                symbolImageView.image = UIImage(systemName: SFSymbols.repos)
+                titleLabel.text = "Public Repos"
+            case .gists:
+                symbolImageView.image = UIImage(systemName: SFSymbols.gists)
+                titleLabel.text = "Public Gists"
+            case .followers:
+                symbolImageView.image = UIImage(systemName: SFSymbols.followers)
+                titleLabel.text = "Followers"
+            case .following:
+                symbolImageView.image = UIImage(systemName: SFSymbols.following)
+                titleLabel.text = "Following"
+        }
+        
+        countLabel.text = "\(count)"
+    }
+    
+
+}
