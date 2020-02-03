c9815aae6c979aa6e96a5d311ab61391bb59f65b
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-02-03 20:58:23 +0300

    create the favorite table view cell

---
+++ GHFollowers/Custom Views/Cells/FavoriteTableViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteTableViewCell.swift
+//  GHFollowers
+//
+//  Created by Abdulaziz AlObaili on 03/02/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    static let reuseId = "FavoriteCollectionViewCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+        
+        // avatarImageView
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        // usernameLabel
+        NSLayoutConstraint.activate([
+            usernameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+
+}
